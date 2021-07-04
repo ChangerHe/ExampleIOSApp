59ffe8c0ba3e693efde36ad055ae865b6e75ee5e
ChangerHe <changerheee@gmail.com>
2021-07-05 00:10:24 +0800

    feat: init project

---
+++ SimpleApp/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  SimpleApp
+//
+//  Created by ChangerHe on 2021/7/4.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SimpleApp/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  SimpleApp
+//
+//  Created by ChangerHe on 2021/7/4.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+// 视图渲染完成的生命周期
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self.view addSubview:({
+        UILabel *label = [[UILabel alloc] init];
+        
+        label.text = @"hello world";
+        [label sizeToFit];
+        label.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
+        label;
+    })];
+}
+
+
+@end
