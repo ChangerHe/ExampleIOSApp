be6ffea5852b4ce24e5e0fc48c32b42cf19edf42
ChangerHe <changerheee@gmail.com>
2021-07-05 23:57:42 +0800

    feat: add UIView demo

--- SimpleApp/ViewController.m
+++ SimpleApp/ViewController.m
@@ -22,7 +22,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSLog(@"view loaded");
-    // Do any additional setup after loading the view.
+    // 初始化一个hello world
     [self.view addSubview:({
         UILabel *label = [[UILabel alloc] init];
         
@@ -31,6 +31,30 @@
         label.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
         label;
     })];
+    
+    // 创建一个红色块, 距离为100, 100, 宽高为100, 100
+    // 初始化UIView, 在视图中申请空间
+    UIView *view = [[UIView alloc] init];
+    // 设置其背景色
+    view.backgroundColor = [UIColor redColor];
+    // 设置其布局方式, x,y为100, 宽高为100
+    view.frame = CGRectMake(100, 100, 100, 100);
+    // 将创建的视图作为子视图, 加入到父视图中
+    [self.view addSubview:view];
+    
+    // 创建一个绿色块, 距离为100, 100, 宽高为100, 100
+    // 初始化UIView, 在视图中申请空间
+    UIView *view2 = [[UIView alloc] init];
+    // 设置其背景色
+    view2.backgroundColor = [UIColor greenColor];
+    // 设置其布局方式, x,y为100, 宽高为100
+    view2.frame = CGRectMake(150, 150, 100, 100);
+    // 将创建的视图作为子视图, 加入到父视图中
+    [self.view addSubview:view2];
+    
+    // view的添加顺序决定了他们直接的覆盖关系, 如上: 绿色色块覆盖住了红色的色块
+    
+    
 }
 
 
